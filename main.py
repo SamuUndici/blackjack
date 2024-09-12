c90a4b5b797bbe758e3d2eae00adacdce3501453
Samu <samuele.placanica@gmail.com>
2024-09-12 15:11:32 +0200

    aggiornati algoritmi

--- main.py
+++ main.py
@@ -1,23 +1,55 @@
 import random
-
-carte = [1 ,2, 3 , 4, 5, 6, 7 , 8, 9,10]
+import time
+carte = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
 carte_usate = []
 banco = []
-#scelgo le due carte del banco 
-#IL BANCO STA A 17 
-carta1b   = random.choice(carte)
-carta2b  = random.choice(carte)
-#scelgo le due carte del giocatore
-carta1g = random.choice(carte)
-carta2g = random.choice(carte)
-print("il banco ha questa carta: ", carta1b)
-print("La tua mano è: ", carta1g , carta2g, ", il risultato è: ", carta1g+carta2g)
 
-gioco = input(print("cosa vuoi fare? Carta (Y) o stai (N)?"))
-if gioco == "Y" or gioco == "y":    
-    print("continuo")
-else: print("non continuo")
- 
+
+
+# Scelgo la prima carta del giocatore
+carta1g = random.choice(carte)
+carte.remove(carta1g)
+
+# Scelgo la carta del banco
+carta1b = random.choice(carte)
+carte.remove(carta1b)  # Rimuovi la carta dalla lista delle carte disponibili
+
+#scelgo la seconda carta del giocatore
+carta2g = random.choice(carte)
+carte.remove(carta2g)
+
+print("Hai questa carta: ", carta1g)
+time.sleep(1.5)
+print("Il banco ha questa carta: ", carta1b)
+time.sleep(1.5)
+print("La tua seconda carta è: ", carta2g)
+time.sleep(1.5)
+
+
+
+# Controllo sul risultato, se è meno di 21 chiedere cosa si vuole fare 
+risultatog = carta1g + carta2g
+print("Il tuo risultato è: ", risultatog)
+if risultatog > 21:
+    print("Hai sballato, il banco vince")
+else:
+    while True:  # Loop per continuare a chiedere finché il giocatore non decide di stare o sballa
+        gioco = input("Cosa vuoi fare? Vuoi un'altra carta (Y) o stai? (N) ").lower()
+        if gioco == "y":
+            nuova_carta = random.choice(carte)  # Pesca una nuova carta
+            carte.remove(nuova_carta)
+            risultatog += nuova_carta
+            print("Hai pescato: ", nuova_carta)
+            print("Il tuo nuovo risultato è: ", risultatog)
+            
+            if risultatog > 21:
+                print("Hai sballato, il banco vince")
+                break
+        elif gioco == "n":
+            
+            break
+        else:
+            print("Inserisci una scelta valida (Y o N)")
 
 #controllo la somma delle carte del giocatore
 #controllo possibili combinazioni (asso + figura = bj , 2+ figura = 2 o 12 ecc)
